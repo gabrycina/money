feab478ac363dce29a05c771a3ca43fae5452b26
gabrycina <c.gabriele.info@gmail.com>
2023-01-06 12:20:09 +0100

    Added videos to Rules screen

--- frontend/lib/providers/game.dart
+++ frontend/lib/providers/game.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unnecessary_getters_setters
+
 import 'package:flutter/material.dart';
 
 class Game with ChangeNotifier {
--- frontend/lib/screens/rules/RulesScreen.dart
+++ frontend/lib/screens/rules/RulesScreen.dart
@@ -1,5 +1,8 @@
-import 'package:animated_button/animated_button.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:provider/provider.dart';
+
+import '../../providers/game.dart';
 
 class RulesScreen extends StatelessWidget {
   /// Creates a [RulesScreen].
@@ -8,6 +11,20 @@ class RulesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Uri getTutorialLink() {
+      String game = Provider.of<Game>(context, listen: false).miniGame;
+
+      if (game == "Min") {
+        return Uri.parse("https://youtu.be/FJSI7QTAt_o?t=812");
+      } else if (game == "Max") {
+        return Uri.parse("https://youtu.be/FJSI7QTAt_o?t=133");
+      } else if (game == "ChoosePrize") {
+        return Uri.parse("https://youtu.be/FJSI7QTAt_o?t=2723");
+      } else {
+        return Uri.parse("https://youtu.be/FJSI7QTAt_o?t=1641");
+      }
+    }
+
     return Scaffold(
         backgroundColor: const Color.fromARGB(255, 60, 42, 69),
         appBar: AppBar(
@@ -17,14 +34,25 @@ class RulesScreen extends StatelessWidget {
           title: const Text("Rules",
               style: TextStyle(fontSize: 30, color: Colors.white)),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(30),
-          child: Center(
-            child: Text(
-              miniGameRules,
-              style: const TextStyle(color: Colors.white, fontSize: 35),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: Center(
+                child: Text(
+                  miniGameRules,
+                  style: const TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
-          ),
+            InkWell(
+                child: const Text(
+                  'Watch tutorial',
+                  style: TextStyle(color: Colors.amberAccent, fontSize: 25),
+                ),
+                onTap: () => launchUrl(getTutorialLink())),
+          ],
         ));
     //);
   }
